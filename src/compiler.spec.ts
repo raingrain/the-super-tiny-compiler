6ed79ceefcc75a26361f2f4a05679c0ba3358c67
raingrain <2711835261@qq.com>
2024-01-24 21:13:00 +0800

    Update compiler.spec.ts

--- src/compiler.spec.ts
+++ src/compiler.spec.ts
@@ -12,7 +12,7 @@ test("compiler", () => {
 test("compiler", () => {
     expect(compiler("(add 2 (subtract 4 2))")).toEqual("add(2, subtract(4, 2));")
 })
+
 test("compiler", () => {
     expect(compiler("(add 2 (subtract 4 2))(add 2 (subtract 4 2))")).toEqual("add(2, subtract(4, 2));\nadd(2, subtract(4, 2));")
 })
-
